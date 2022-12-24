79b716cc51bf5efb24515831aff9beb8810712fe
dzhuraa <d.zhurakivsky@gmail.com>
2022-12-24 12:43:55 +0200

    created index.html and main.js added some users from jsonplaceholder added link on user-details.html for each user created user-details.html and user-details.js

---
+++ main.js
@@ -0,0 +1,19 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((value) => {
+        let data = value
+        console.log(data);
+        for (const user of data) {
+            let div = document.createElement('div');
+            div.classList.add('user-box');
+            let h2 = document.createElement('h2');
+            h2.innerText = `${user.id} ${user.name}`;
+            let btn = document.createElement('button');
+            btn.innerText = 'About user';
+            let a = document.createElement('a');
+            a.href = 'user-details.html?id=' + user.id;
+            a.appendChild(btn);
+            div.append(h2, a);
+            document.body.appendChild(div);
+        }
+    });
